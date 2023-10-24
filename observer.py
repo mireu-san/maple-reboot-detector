0a3bcdd72aec6bb7e2d6eeba15717fff0f0beb29
Chesed Kim <starmireu@gmail.com>
2023-10-24 14:20:07 +0900

    refactor - observer.py def -> class

--- observer.py
+++ observer.py
@@ -2,40 +2,45 @@ import requests
 from bs4 import BeautifulSoup
 import time
 from win11toast import toast
-from datetime import datetime, timedelta
 
 
-# Check if keyword exists in the first page of the url
-def check_keyword(url, keyword):
-    response = requests.get(url)
-    response.encoding = "utf-8"
-    soup = BeautifulSoup(response.text, "html.parser")
-    for li in soup.select("div.news_board ul li"):
-        a_text = li.find("a").text if li.find("a") else ""
-        if keyword in a_text:
-            return True  # Return True if keyword found
-    return False  # Return False if no matching keyword found
+class KeywordChecker:
+    def __init__(self, url, keyword):
+        self.url = url
+        self.keyword = keyword
+
+    # Method to check for keyword in the provided URL
+    def check_keyword(self):
+        response = requests.get(self.url)
+        response.encoding = "utf-8"
+        soup = BeautifulSoup(response.text, "html.parser")
+        for li in soup.select("div.news_board ul li"):
+            a_text = li.find("a").text if li.find("a") else ""
+            if self.keyword in a_text:
+                return True
+        return False
+
+    # Method to check for keyword in the provided URL
+    def send_notification(self, message):
+        toast(
+            "Keyword Found",
+            message,
+            on_click="https://maplestory.nexon.com/News/Notice/Notice",
+        )
 
 
-# Send alarm to Windows 11's notification center
-def send_notification(message):
-    toast(
-        "Keyword Found",
-        message,
-        on_click="https://maplestory.nexon.com/News/Notice/Notice",
-    )
+if __name__ == "__main__":
+    url = "https://maplestory.nexon.com/News/Notice/Notice"
+    keyword = "리부트"
+    checker = KeywordChecker(url, keyword)
 
-
-url = "https://maplestory.nexon.com/News/Notice/Notice"
-keyword = "리부트"
-
-# Message to be sent to Windows 11's notification center
-while True:
-    if check_keyword(url, keyword):
-        send_notification("리부트 공지가 있습니다!")
-        print("Keyword found from the first page. See url.")
-        break
-    else:
-        send_notification("리부트 공지는 안 보입니다.")
-        print("Keyword not found from the first page. See url.")
-    time.sleep(86400)
+    # Infinite loop to keep checking for the keyword at the specified URL
+    while True:
+        if checker.check_keyword():
+            checker.send_notification("리부트 공지가 있습니다!")
+            print("Keyword found from the first page. See url.")
+            break
+        else:
+            checker.send_notification("리부트 공지는 안 보입니다.")
+            print("Keyword not found from the first page. See url.")
+        time.sleep(14400)  # Set as 4hrs to avoid spamming the target url.
